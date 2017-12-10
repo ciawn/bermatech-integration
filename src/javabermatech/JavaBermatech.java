0efd1c7d5a066c982d7194da4f1c45c92d11c913
Reginaldo Junior <76regi@gmail.com>
2017-12-10 19:17:29 -0200

    add repeat method to print file

--- src/javabermatech/JavaBermatech.java
+++ src/javabermatech/JavaBermatech.java
@@ -7,6 +7,7 @@ package javabermatech;
 import javabermatech.ImpressaoNaoFiscal;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.Timer;
 
 /**
  *
@@ -17,18 +18,22 @@ public class JavaBermatech {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        ImpressaoNaoFiscal ImpressaoNaoFiscalObj = new ImpressaoNaoFiscal();
+    public static void main(String[] args) throws InterruptedException {
+        boolean repeat = true;
         
-        List<String> listaImpressoras = ImpressaoNaoFiscalObj.retornaImpressoras();
-        
-        System.out.println(listaImpressoras.get(2));
-        
-        ImpressaoNaoFiscalObj.detectaImpressoras(listaImpressoras.get(2));
-        
-        System.out.println(ImpressaoNaoFiscalObj.imprime("Ei"));
-        
-        // TODO code application logic here
+        while (repeat) {
+            Thread.currentThread().sleep(5000);        
+
+            ImpressaoNaoFiscal ImpressaoNaoFiscalObj = new ImpressaoNaoFiscal();
+
+            List<String> listaImpressoras = ImpressaoNaoFiscalObj.retornaImpressoras();
+
+            System.out.println(listaImpressoras.get(2));
+
+//            ImpressaoNaoFiscalObj.detectaImpressoras(listaImpressoras.get(2));
+
+//            System.out.println(ImpressaoNaoFiscalObj.imprime("Ei"));
+        }
     }
     
 }
